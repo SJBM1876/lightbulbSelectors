5a6a39af472bc98be5728a4b7d2daf19c219da13
SJBM1876 <137792828+SJBM1876@users.noreply.github.com>
2024-09-25 17:24:17 +0100

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-// Write your code here
+// Select lightbulbs.
+
+const lightbulbs = document.querySelector('.lightbulb');
+
+// Create count variable. Assign value to 0.
+
+let count = 0;
+
+// Select subtitle to update count.
+
+const subtitle = document.querySelector('.subtitle');
+
+
+// Add click event listener to all lightbulbs
+lightbulbs.addEventListener('click', function() {
+      
+      lightbulbs.classList.toggle('active');
+
+    //Increase count
+
+    count++;
+
+// If statement to display singular or plural.
+    if (count === 1) {
+        subtitle.innerHTML = `You've clicked the lights 1 time`;
+      } else {
+        subtitle.innerHTML = `You've clicked the lights ${count} times`;
+      }
+});
+
+
